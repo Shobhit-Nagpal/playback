bcc2e32d6242ecab60fa47016f1864c445a9ce33
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2025-01-02 14:14:04 +0530

    draw hand connections

--- play.py
+++ play.py
@@ -6,17 +6,19 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, value=600)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, value=800)
 
+mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 hand = mp_hands.Hands()
 
 while True:
-    success, frame = cap.read()
+    success, frame = cap.read()  # Gives BGR
     if success:
         RGB_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        result = hand.process(RGB_frame)
+        result = hand.process(RGB_frame)  # Takes in RGB
         if result.multi_hand_landmarks:
             for hand_landmarks in result.multi_hand_landmarks:
                 print(hand_landmarks)
+                mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
         cv2.imshow(winname="capture image", mat=frame)
         if cv2.waitKey(1) == ord("q"):
